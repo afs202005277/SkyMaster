860a7968161f6afa82521f540b99c8bd74e993eb
Pedro Fonseca <pedrojgfonseca@gmail.com>
2021-12-13 10:34:49 +0000

    Added class Bilhete

---
+++ src/Bilhete.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Pedro Fonseca on 13/12/2021.
+//
+
+#include "Bilhete.h"
+
---
+++ src/Bilhete.h
@@ -0,0 +1,17 @@
+//
+// Created by Pedro Fonseca on 13/12/2021.
+//
+
+#ifndef PROJETO1_BILHETE_H
+#define PROJETO1_BILHETE_H
+
+
+class Bilhete
+{
+private:
+    unsigned numMalas;
+    bool checkInAuto;
+};
+
+
+#endif //PROJETO1_BILHETE_H
